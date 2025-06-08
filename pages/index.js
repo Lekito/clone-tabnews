dd2ffeba03cd3ecd9948f9363c4ca8628b34a2bc
Lekito <alexlekito075@gmail.com>
2025-06-08 01:19:01 -0300

    feat: add text

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 
 function Home() {
     return(
-        <h1>Lekito ama ser Dev!</h1>
+        <header>
+            <h1>Lekito ama ser Dev!</h1>
+            <h2>Vou avançar mais um passo, rumo a ser um excelente desenvolvedor!</h2>
+        </header>
     )
 }
 
