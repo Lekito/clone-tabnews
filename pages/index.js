89977fb8d2d987583f270cd45da832a20f332f2e
Lekito <alexlekito075@gmail.com>
2025-06-12 01:39:15 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-
 function Home() {
-    return(
-        <header>
-            <h1>Lekito ama ser Dev!</h1>
-            <h2>Vou avançar mais um passo, rumo a ser um excelente desenvolvedor!</h2>
-        </header>
-    )
+  return (
+    <header>
+      <h1>Lekito ama ser Dev!</h1>
+      <h2>Vou avançar mais um passo, rumo a ser um excelente desenvolvedor!</h2>
+    </header>
+  );
 }
 
 export default Home;
